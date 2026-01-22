5c83bcf1aab6da587f1023537270cef2e6029ed9
krish <rokrish@calpoly.edu>
2026-01-22 15:42:35 -0800

    Added csv file reader function and worked on testcases for helper functions

--- co2_stats.py
+++ co2_stats.py
@@ -25,23 +25,31 @@ class RLNode:
     first: Rows
     rest: LinkedList
 
+#Converts a string to a float, but returns none if the string is blank
+def str_to_float(s: str) -> float | None:
+    if s == "":
+        return None
+    return float(s)
+
 #converts arrays of strings to Row objects
 def array_to_row(data: list[str]) -> Rows:
-    return Rows(data[0], int(data[1]), float(data[2]),float(data[3]), float(data[4]), float(data[5]), float(data[6]), float(data[7]))
+    return Rows(data[0], int(data[1]), 
+        str_to_float(data[2]),str_to_float(data[3]), str_to_float(data[4]), str_to_float(data[5]), str_to_float(data[6]), str_to_float(data[7]))
     
 file: TypeAlias = Literal["sample-file.csv", "some_ghg_emissions.csv"]
 #parses csv file and returns all data in a row collectively in a LinkedList
-def read_csv_lines(file_name: file)-> LinkedList:
-    'expected_labels : List[str] = ['item','price','number in stock']'
-    with open(file_name, newline="") as csvfile:
-        iter = csv.reader(csvfile)
-        topline : List[str] = next(iter)
-        if not (topline == expected_labels):
-            raise ValueError(f"unexpected first line: got: {topline}")
-    Dataset = []
-    for line in iter:
-        Dataset.append(line)
-    return Dataset
+def read_csv_lines(filename: file)-> list[list[str]]:
+    rows: list[list[str]] = []
+
+    with open(filename, newline="") as csvfile:
+        reader = csv.reader(csvfile)
+
+        _ = next(reader)  # skip header
+
+        for fields in reader:
+            rows.append(fields)
+
+    return rows
 
 
 #returns the length of a LinkedList of row objects
@@ -53,12 +61,31 @@ def listlen(ll: LinkedList) ->int:
         return 1+ listlen(rest_of_list)
     
 #FIX LATER
-def filter(ll:LinkedList, )
+def filter(ll:LinkedList,):
+    pass
+
+sample_ll: LinkedList = RLNode(Rows("1", 2000, None, None, None, None, None, None), 
+                        RLNode(Rows("1", 2000, None, None, None, None, None, None), 
+                        RLNode(Rows("1", 2000, None, None, None, None, None, None), None)))
+sample_ll_2:LinkedList = RLNode(Rows("1", 2000, None, None, None, None, None, None), None)
+sample_ll_3:LinkedList = None
 
 class Tests(unittest.TestCase):
-    pass
-    'def test_array_to_row(self): , def test_read_csv-lines(self):, def test_listlen(self):'
-      
+    
+    'def test_array_to_row(self): , def test_read_csv-lines(self):,'
+    def test_str_to_float(self):
+        self.assertEqual(str_to_float("3434"), 3434.0)
+        self.assertEqual(str_to_float("23.32"), 23.32)
+        self.assertEqual(str_to_float("0.3214"), 0.3214)
+        self.assertEqual(str_to_float(".00001"), .00001)
+
+    def test_list_len(self):
+        self.assertEqual(listlen(sample_ll), 3)
+        self.assertEqual(listlen(sample_ll_2), 1)
+        self.assertEqual(listlen(sample_ll_3), 0)
+
+
+
 
 if (__name__ == '__main__'):
     unittest.main()
